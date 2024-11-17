264487469889a1c1533d5f0891716141c6920dfe
Sumit Kumar Jha <o1sumeetkumar@gmail.com>
2024-11-17 11:04:44 +0530

    Integrates React Navigation
    Adds React Navigation for app navigation.  Simplifies the app structure by removing unnecessary components and styles. Updates Android's `MainActivity` to correctly handle the application lifecycle.  Configures Babel to use module aliases for improved code organization.

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,37 @@
 module.exports = {
   presets: ['module:@react-native/babel-preset'],
+  plugins: [
+    [
+      'module-resolver',
+      {
+        extensions: [
+          '.ios.js',
+          '.android.js',
+          '.ios.jsx',
+          '.android.jsx',
+          '.js',
+          '.jsx',
+          '.json',
+          '.ts',
+          '.tsx',
+        ],
+        root: ['.'],
+        alias: {
+          '@api': './src/services/apis',
+          '@assets': './src/assets',
+          '@components': './src/components',
+          '@scenes': './src/scenes',
+          '@screens': './src/screens',
+          '@themes': './src/themes',
+          '@utils': './src/utils',
+          '@hooks': './src/hooks',
+          '@layouts': './src/layouts',
+          '@types': './src/types',
+          '@toolkit': './src/toolkit',
+          '@functions': './src/functions',
+          '@interfaces': './src/interfaces',
+        },
+      },
+    ],
+  ],
 };
